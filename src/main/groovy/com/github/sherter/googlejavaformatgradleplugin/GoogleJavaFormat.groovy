c77b9fec9e20d4040e325b6732e5ba110c2104cd
Simon Herter <sim.herter@gmail.com>
2016-02-15 12:13:02 +0100

    Format multiple files concurrently

--- src/main/groovy/com/github/sherter/googlejavaformatgradleplugin/GoogleJavaFormat.groovy
+++ src/main/groovy/com/github/sherter/googlejavaformatgradleplugin/GoogleJavaFormat.groovy
@@ -4,8 +4,13 @@ import groovy.transform.PackageScope
 import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 class GoogleJavaFormat extends SourceTask {
 
+    private static final MAX_THREADS = 20;
     /**
      * The same instance will be injected into all tasks of this type
      * (see {@link GoogleJavaFormatPlugin#createAndInjectFileStateHandler}).
@@ -23,11 +28,22 @@ class GoogleJavaFormat extends SourceTask {
     @TaskAction
     void formatSources() {
         def formatter = constructFormatter()
-        source.each { file ->
-            fileStateHandler.formatIfNotUpToDate(file) { fileToFormat ->
-                fileToFormat.write(formatter.formatSource(fileToFormat.text))
+        Set<File> sourceFiles = source.getFiles()
+        int numThreads = Math.min(sourceFiles.size(), MAX_THREADS)
+        Executor executor = Executors.newFixedThreadPool(numThreads)
+
+        sourceFiles.each { file ->
+            executor.execute {
+                fileStateHandler.formatIfNotUpToDate(file) { fileToFormat ->
+                    fileToFormat.write(formatter.formatSource(fileToFormat.text))
+                }
             }
         }
+        executor.shutdown()
+
+        // blocks forever, unit is ignored if timeout is MAX_VALUE
+        // (see http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/package-summary.html)
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
     }
 
     private constructFormatter() {
