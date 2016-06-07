92bdfed23fe1276fd7036547e95cdac2f978a800
Simon Herter <sim.herter@gmail.com>
2016-06-07 19:09:01 +0200

    Extend option tests to custom tasks

--- src/integTest/groovy/com/github/sherter/googlejavaformatgradleplugin/OptionsSpec.groovy
+++ src/integTest/groovy/com/github/sherter/googlejavaformatgradleplugin/OptionsSpec.groovy
@@ -1,10 +1,10 @@
 package com.github.sherter.googlejavaformatgradleplugin
 
-import org.gradle.testkit.runner.UnexpectedBuildFailure
+import static com.github.sherter.googlejavaformatgradleplugin.GoogleJavaFormatPlugin.DEFAULT_FORMAT_TASK_NAME
 
 class OptionsSpec extends AbstractIntegrationSpec {
 
-    def 'format and verify using different styles'() {
+    def 'default format task incorporates specified style'() {
         given:
         def foo = project.createFile(['Foo.java'], 'class   Foo   { void bar()  {}}')
         project.createFile(['build.gradle'], """\
@@ -14,28 +14,86 @@ class OptionsSpec extends AbstractIntegrationSpec {
             |""".stripMargin())
 
         when:
-        runner.withArguments('verGJF').build()
+        runner.withArguments(DEFAULT_FORMAT_TASK_NAME).build()
 
         then:
-        thrown(UnexpectedBuildFailure)
-
-        when:
-        runner.withArguments('goJF').build()
-
-        then:
-        foo.lastModifiedTimeHasChanged()
-        foo.contentHasChanged()
         foo.read() == expected
-
-        when:
-        runner.withArguments('verGJF').build()
-
-        then:
-        notThrown(UnexpectedBuildFailure)
 
         where:
         extension                                      | expected
-        ""                                             | "class Foo {\n  void bar() {}\n}\n"
+        ""                                             | "class Foo {\n  void bar() {}\n}\n" // == GOOGLE
+        "googleJavaFormat { options style: 'GOOGLE' }" | "class Foo {\n  void bar() {}\n}\n"
+        "googleJavaFormat { options style: 'AOSP' }"   | "class Foo {\n    void bar() {}\n}\n"
+    }
+
+    def 'custom format task incorporates specified style'() {
+        given:
+        def foo = project.createFile(['Foo.java'], 'class   Foo   { void bar()  {}}')
+        project.createFile(['build.gradle'], """\
+            |$applyPlugin
+            |$defaultRepositories
+            |task myFormat(type: ${GoogleJavaFormat.name}) {
+            |  source 'Foo.java'
+            |}
+            |$extension
+            |""".stripMargin())
+
+        when:
+        runner.withArguments('myFormat').build()
+
+        then:
+        foo.read() == expected
+
+        where:
+        extension                                      | expected
+        ""                                             | "class Foo {\n  void bar() {}\n}\n" // == GOOGLE
+        "googleJavaFormat { options style: 'GOOGLE' }" | "class Foo {\n  void bar() {}\n}\n"
+        "googleJavaFormat { options style: 'AOSP' }"   | "class Foo {\n    void bar() {}\n}\n"
+    }
+
+    def 'default verify task incorporates specified style'() {
+        given:
+        def foo = project.createFile(['Foo.java'], content)
+        project.createFile(['build.gradle'], """\
+            |$applyPlugin
+            |$defaultRepositories
+            |$extension
+            |""".stripMargin())
+
+        when:
+        def result = runner.withArguments(DEFAULT_FORMAT_TASK_NAME).build()
+
+        then:
+        result.output =~ /BUILD SUCCESSFUL/
+
+        where:
+        extension                                      | content
+        ""                                             | "class Foo {\n  void bar() {}\n}\n" // == GOOGLE
+        "googleJavaFormat { options style: 'GOOGLE' }" | "class Foo {\n  void bar() {}\n}\n"
+        "googleJavaFormat { options style: 'AOSP' }"   | "class Foo {\n    void bar() {}\n}\n"
+    }
+
+    def 'custom verify task incorporates specified style'() {
+        given:
+        def foo = project.createFile(['Foo.java'], content)
+        project.createFile(['build.gradle'], """\
+            |$applyPlugin
+            |$defaultRepositories
+            |$extension
+            |task myVerify(type: ${VerifyGoogleJavaFormat.name}) {
+            |  source 'Foo.java'
+            |}
+            |""".stripMargin())
+
+        when:
+        def result = runner.withArguments('myVerify').build()
+
+        then:
+        result.output =~ /BUILD SUCCESSFUL/
+
+        where:
+        extension                                      | content
+        ""                                             | "class Foo {\n  void bar() {}\n}\n" // == GOOGLE
         "googleJavaFormat { options style: 'GOOGLE' }" | "class Foo {\n  void bar() {}\n}\n"
         "googleJavaFormat { options style: 'AOSP' }"   | "class Foo {\n    void bar() {}\n}\n"
     }
