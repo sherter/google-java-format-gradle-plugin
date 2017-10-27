ae9f7795c3eab5bf4310a5491cd94dc6007ebaaf
Jesse Kinkead <jesse.kinkead@nordstrom.com>
2017-10-27 10:45:22 -0700

    Make updates to orderImports trigger a reformat.

--- src/main/java/com/github/sherter/googlejavaformatgradleplugin/FormatterOptions.java
+++ src/main/java/com/github/sherter/googlejavaformatgradleplugin/FormatterOptions.java
@@ -8,12 +8,15 @@ import javax.annotation.Nullable;
 @AutoValue
 abstract class FormatterOptions {
 
-  static FormatterOptions create(@Nullable String version, ImmutableSet<FormatterOption> options) {
-    return new AutoValue_FormatterOptions(version, options);
+  static FormatterOptions create(
+      @Nullable String version, ImmutableSet<FormatterOption> options, boolean orderImports) {
+    return new AutoValue_FormatterOptions(version, options, orderImports);
   }
 
   @Nullable
   abstract String version();
 
   abstract ImmutableSet<FormatterOption> options();
+
+  abstract boolean orderImports();
 }
