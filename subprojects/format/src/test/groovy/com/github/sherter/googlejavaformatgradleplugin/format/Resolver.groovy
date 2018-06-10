a39de7a954122477d92233a4d237e2e426109538
Simon Herter <sim.herter@gmail.com>
2018-06-10 14:47:35 +0200

    Replace grape with maven-resolver for resolving gjf artifacts in tests
    Downloading artifacts with groovy's 'grape' didn't work
    anymore (perhaps because we are using an old version of groovy to
    support older Gradle versions?). Using 'maven-resolver', formerly
    known as (eclipse-) 'aether', fixes the problem.

--- subprojects/format/src/test/groovy/com/github/sherter/googlejavaformatgradleplugin/format/Resolver.groovy
+++ subprojects/format/src/test/groovy/com/github/sherter/googlejavaformatgradleplugin/format/Resolver.groovy
@@ -1,14 +1,66 @@
 package com.github.sherter.googlejavaformatgradleplugin.format
 
-import groovy.grape.Grape
+import org.apache.maven.repository.internal.MavenRepositorySystemUtils
+import org.eclipse.aether.DefaultRepositorySystemSession
+import org.eclipse.aether.RepositorySystem
+import org.eclipse.aether.RepositorySystemSession
+import org.eclipse.aether.artifact.DefaultArtifact
+import org.eclipse.aether.collection.CollectRequest
+import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory
+import org.eclipse.aether.graph.Dependency
+import org.eclipse.aether.impl.DefaultServiceLocator
+import org.eclipse.aether.repository.LocalRepository
+import org.eclipse.aether.repository.RemoteRepository
+import org.eclipse.aether.repository.RepositoryPolicy
+import org.eclipse.aether.resolution.ArtifactResult
+import org.eclipse.aether.resolution.DependencyRequest
+import org.eclipse.aether.spi.connector.RepositoryConnectorFactory
+import org.eclipse.aether.spi.connector.transport.TransporterFactory
+import org.eclipse.aether.transport.file.FileTransporterFactory
+import org.eclipse.aether.transport.http.HttpTransporterFactory
+import org.eclipse.aether.util.artifact.JavaScopes
 
 class Resolver {
 
+    private static final RemoteRepository MAVEN_CENTRAL_REPOSITORY = new RemoteRepository.Builder("central", "default", "https://repo.maven.apache.org/maven2/").build()
+    private static final LocalRepository LOCAL_REPOSITORY = new LocalRepository(System.getProperty("maven.repo.local"))
+
+    private static final RepositorySystem system = createRepositorySystem()
+    private static final RepositorySystemSession session = createRepositorySystemSession(system)
+
     static ClassLoader resolve(String version) {
-        ClassLoader loader = new GroovyClassLoader(ClassLoader.getSystemClassLoader())
-        Grape.grab([classLoader: loader], [group: Gjf.GROUP_ID,
-                                           module: Gjf.ARTIFACT_ID,
-                                           version: version])
-        return loader
+        DependencyRequest request = createGoogleJavaFormatRequest(version)
+        List<ArtifactResult> artifactResults = system.resolveDependencies(session, request).getArtifactResults()
+
+        URL[] artifactLocations = artifactResults.collect {
+            result -> result.getArtifact().getFile().toURI().toURL()
+        }
+        URLClassLoader classLoader = new URLClassLoader(artifactLocations, (ClassLoader) null)
+        return classLoader
+    }
+
+
+    private static DependencyRequest createGoogleJavaFormatRequest(String version) {
+        def coordinates = new DefaultArtifact("${Gjf.GROUP_ID}:${Gjf.ARTIFACT_ID}:${version}")
+        def collectRequest = new CollectRequest()
+                .setRoot(new Dependency(coordinates, JavaScopes.RUNTIME))
+                .setRepositories([MAVEN_CENTRAL_REPOSITORY])
+        return new DependencyRequest().setCollectRequest(collectRequest)
+    }
+
+    private static RepositorySystem createRepositorySystem() {
+        DefaultServiceLocator locator = MavenRepositorySystemUtils.newServiceLocator()
+        locator.addService(RepositoryConnectorFactory.class, BasicRepositoryConnectorFactory.class)
+        locator.addService(TransporterFactory.class, FileTransporterFactory.class)
+        locator.addService(TransporterFactory.class, HttpTransporterFactory.class)
+
+        return locator.getService(RepositorySystem.class)
+    }
+
+    private static RepositorySystemSession createRepositorySystemSession(RepositorySystem system) {
+        DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession()
+        session.setUpdatePolicy(RepositoryPolicy.UPDATE_POLICY_NEVER)
+        session.setLocalRepositoryManager(system.newLocalRepositoryManager(session, LOCAL_REPOSITORY))
+        return session
     }
 }
